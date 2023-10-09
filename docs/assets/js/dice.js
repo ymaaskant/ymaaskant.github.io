4161841dab2c88221c42d008cb1f1974fd320a87
ymaaskant <y.maaskant@gmail.com>
2023-10-09 17:22:57 +0200

    game

---
+++ docs/assets/js/dice.js
@@ -0,0 +1,27 @@
+var dice = {
+    sides: 6,
+    roll: function () {
+      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+      return randomNumber;
+      console.log(randomNumber);
+    }
+  }
+  
+  
+  
+  //Prints dice roll to the page
+  
+  function printNumber(number) {
+    // var placeholder = document.getElementById('placeholder');
+    // placeholder.innerHTML = number;
+    console.log(number);
+  }
+  
+  var button = document.getElementById('button');
+  
+  button.onclick = function() {
+    var result = dice.roll();
+    printNumber(result);
+  };
+  
+  
