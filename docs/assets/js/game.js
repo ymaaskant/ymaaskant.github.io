7a00fae74652183ef8b2554d867f72eb5aacbbca
ymaaskant <y.maaskant@gmail.com>
2023-10-24 21:11:29 +0200

    update index + game.js

--- docs/assets/js/game.js
+++ docs/assets/js/game.js
@@ -1,36 +1,7 @@
-
-// UI handling
-const disableButton = () => {
-  const button = document.querySelector("#buttonRoll");
-  button.disabled = true;
-};
-
-const enableButton = () => {
-  const button = document.querySelector("#buttonRoll");
-  button.disabled = false;
-};
-
-// Player Objects
-const playerObj = () => {
-  return {
-    name:"player",
-    position:0,
-    selector:"#player0"
-  };
-};
-
 var gameStarted = false;
 var playerTurn = 1;
 var players =  [];
 var nPlayers = 8;
-
-// To start the game
-const startGame = (nPlayers) => {
-  players = Array(nPlayers).fill(null).map(playerObj);
-  for (var i = 0; i < nPlayers; i++){
-    players[i].selector=players[i].selector+(i+1);
-  }
-};
 
 // Board 
 const boardSpot = [
@@ -59,50 +30,107 @@ const boardSpot = [
 ];
 
 const dice = [
-  {value:1},{value:1},{value:2},{value:3},{value:4},{value:0}
+  1,1,2,3,4,0
 ];
+
+// UI handling
+const disableButton = () => {
+  const button = document.querySelector("#buttonRoll");
+  button.disabled = true;
+};
+
+const enableButton = () => {
+  const button = document.querySelector("#buttonRoll");
+  button.disabled = false;
+};
+
+// Player Objects
+const playerObj = () => {
+  return {
+    name:"player",
+    position:0,
+    selector:"#player0"
+  };
+};
+
+// To start the game
+const startGame = (nPlayers) => {
+  players = Array(nPlayers).fill(null).map(playerObj);
+  for (var i = 0; i < nPlayers; i++){
+    players[i].selector=players[i].selector+(i+1);
+  }
+};
+
+const playerTurnHandler = () => {
+  if (playerTurn >= nPlayers){
+    playerTurn=1;
+  } 
+  else playerTurn++;
+};
 
 const game = () => {
 // Initialize
   if(gameStarted === false){
     startGame(8);
-    console.log(players);
+    // console.log(players);
     gameStarted=true;
   }
 
   // roll
   disableButton();
-  var playerRoll=roll();
-  var playerPosition = calcPos(playerTurn-1,playerRoll)
+  var playerRoll = roll();
+  console.log("playerRoll in game(): " + playerRoll)
+switch(playerRoll){
+  case 0:
+    trapActivation();
+    break;
+default:
+  var playerPosition = calcPos(playerTurn-1,playerRoll);
   playerMove(playerTurn-1, playerPosition);
-
-
+}
+  
   // Next Turn
-  if (playerTurn >= nPlayers){
-    playerTurn=1;
-  } 
-  else playerTurn++;
-enableButton();
+  playerTurnHandler();
+  enableButton();
 };
 
-const trapActivation = (nTraps) =>{
-  var randomNumber = Math.floor(Math.random() * nTraps) + 1;
-  // console.log(randomNumber);
-  return(randomNumber);
+const trapActivation = () =>{
+  var randomNumber = Math.floor(Math.random() * 4) + 1;
+  switch (randomNumber){
+    case 1:
+      console.log("trap 1 activated");
+      for (let i = 0; i < nPlayers-1;i++){
+        if (players[i].position > 3 && players[i].position <10){
+          console.log(players[i].name + " is reset to spot 3");
+          playerMove(i,3);
+        }
+      }
+      break;
+  case 2:
+    console.log("trap 2 activated");
+    break;
+  case 3:
+    console.log("trap 3 activated");
+    break;
+  case 4:
+    console.log("trap 4 activated")
+    break;
+  }
 };
 
 const roll = () => {
-  var randomNumber = Math.floor(Math.random() * 6) + 1;
-  // console.log("rolled: " + randomNumber);
-  return randomNumber;
+  var randomNumber = Math.floor(Math.random() * 6);
+  var diceSide = parseInt(dice[randomNumber]);
+  console.log("diceSide in roll(): " + diceSide)
+  return diceSide;
+  // console.log("diceroll: " + diceSide);
 };
 
 const calcPos = (playerId,roll) => {
   var playerNewPosition = players[playerId].position + roll;
-  console.log(playerNewPosition);
-  for (var i = 0; i < nPlayers; i++){
+  // console.log("playerNewPosition in calcPos: " + playerNewPosition);
+  for (var i = 0; i < nPlayers-1; i++){
     if(i === playerId){
-      console.log(i);
       continue;
     }
     if(players[i].position === playerNewPosition){
@@ -115,7 +143,14 @@ const calcPos = (playerId,roll) => {
 
 const animationTimer = (ms) => new Promise((res) => setTimeout(res, ms));
 
+async function playerReset(resetSpot){
+
+};
+
 async function playerMove(playerId,playerNewPosition){
+  if(!playerNewPosition === players[playerId].position){
+    return;
+  }
   var player = document.querySelector(players[playerId].selector);
 
 // Movement animation
@@ -142,4 +177,14 @@ async function playerMove(playerId,playerNewPosition){
     z: boardSpot[playerNewPosition].y
   });
   players[playerId].position=playerNewPosition;
+
+  console.log("playerNewPosition in playermove(): " + playerNewPosition);
+  console.log("boardspot value at new positio: " + boardSpot[playerNewPosition].type);
+  if(! boardSpot[playerNewPosition].type === "trap"){
+    console.log("no trap at position");
+    return;
+  }
+  if(boardSpot[playerNewPosition].type === "trap"){
+    trapActivation();
+  }
 };
